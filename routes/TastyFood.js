2ad10b9f1cc2cd1d186b3fc997ceefcddb264df5
ppranitharedd <policepatelpranitha03@gmail.com>
2023-10-30 11:00:35 -0500

    changes in tastyfood.js file

--- routes/TastyFood.js
+++ routes/TastyFood.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results Tasty Foods' });
+  res.render('TastyFood', { title: 'Search Results Tasty Foods' });
 });
 
 module.exports = router;
