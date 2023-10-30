8db0ad6fbbe71d5956e28f5b06bb1d7b84f40367
ppranitharedd <policepatelpranitha03@gmail.com>
2023-10-30 11:59:23 -0500

    choose update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tastyfoodRouter = require('./routes/TastyFood');
 var boardRouter=require('./routes/board');
+var chooseRouter=require('./routes/choose');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/TastyFood',tastyfoodRouter);
 app.use('/board',boardRouter);
+app.use('/choose',choose);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
